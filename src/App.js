6d4c6614d2a5400eebc27c28b5f9f905f22e3fbb
Ashn20 <ashn200820@gmailcom>
2023-11-29 15:26:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import PhotoGallery from './pages/PhotoGallery';
+import Navbar from './components/Navbar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<PhotoGallery/>}/>
+        <Route path="/photo-gallery" element={<PhotoGallery/>}/>
+        <Route path="/signup" element={<PhotoGallery/>}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
